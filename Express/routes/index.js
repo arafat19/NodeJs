388676c70eb8c4da34f35a46407dd9f2aa736ad3
arafat19 <arafat0112358@gmail.com>
2015-09-21 14:08:08 +0600

    Experss

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var vd = require('../videodata.json');
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    port     : 8889,
+    database : 'test_db'
+});
+
+connection.connect();
+
+var article ={
+    author: 'Md Ibrahim Arafat',
+    title: 'Mysql + NodeJs',
+    body: 'Huge'
+};
+
+/*var query = connection.query('INSERT INTO articles SET ?', article, function(err, result){
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.error(result);
+});*/
+var id;
+var query = connection.query('SELECT * FROM articles', function(err, result){
+    if(err){
+        console.error(err);
+        return;
+    }
+   //console.error(result);
+    //console.log(result[0].id);
+    id = result;
+    //res = result;
+});
+
+//var data = JSON.parse(res);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
       title: 'Limited Express',
       name: 'Arafat',
-      videodata: vd
+      data: id
   });
 });
 
